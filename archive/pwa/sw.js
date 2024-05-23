3ea0dd54423fb2dbc7e6e92352a1f175508d9ef6
Đàm Minh Giang <copecute10x@gmail.com>
2024-05-23 15:52:20 +0700

    Update sw.js

--- archive/pwa/sw.js
+++ archive/pwa/sw.js
@@ -1,55 +1,21 @@
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received');
+  const title = 'Thông báo đến từ ứng dụng';
+  const options = {
+    body: event.data.text(),
+    icon: '//blogger.googleusercontent.com/img/a/AVvXsEiYorgTwvKTp7bjT_1O6HrAl2K4vYEcimlyzfv-0UNwF8x_ov7avCHuZoVdg6K-u2GhL7bOUOmL9DSC4YiBQOF82bmOxYFhmzcd_S15-AikwfL83vmYIAPuBtCPGeRsRfAiVw0REdGk-GZltwNDSWuKC-WFGvU1WwUCASD8CynnsGpOH91geRjUW2rVmC0=w301-h154-p-k-no-nu',
+    badge: '//cdn.minhgiang.pro/archive/favicon/android-icon-192x192.png'
+  };
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-if (workbox) {
-workbox.core.skipWaiting();
-workbox.core.clientsClaim();
-workbox.core.setCacheNameDetails({
-  prefix: 'thn-sw',
-  suffix: 'v22',
-  precache: 'install-time',
-  runtime: 'run-time'
+  event.waitUntil(self.registration.showNotification(title, options));
 });
 
-const FALLBACK_HTML_URL = 'https://cdn.minhgiang.pro/archive/pwa/offline.html';
-const version = workbox.core.cacheNames.suffix;
-workbox.precaching.precacheAndRoute([{url: FALLBACK_HTML_URL, revision: null},{url: 'https://cdn.minhgiang.pro/archive/pwa/manifest.json', revision: null},{url: '/favicon.ico', revision: null}]);
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
 
-workbox.routing.setDefaultHandler(new workbox.strategies.NetworkOnly());
+  event.notification.close();
 
-workbox.routing.registerRoute(
-    new RegExp('.(?:css|js|png|gif|jpg|svg|ico)$'),
-    new workbox.strategies.CacheFirst({
-        cacheName: 'images-js-css-' + version,
-        plugins: [
-            new workbox.expiration.ExpirationPlugin({
-                maxAgeSeconds: 60 * 24 * 60 * 60,
-                maxEntries:200,
-                purgeOnQuotaError: true
-            })
-        ],
-    }),'GET'
-);
-
-workbox.routing.setCatchHandler(({event}) => {
-      switch (event.request.destination) {
-        case 'document':
-        return caches.match(FALLBACK_HTML_URL);
-      break;
-      default:
-        return Response.error();
-  }
-});
-
-self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches
-      .keys()
-      .then(keys => keys.filter(key => !key.endsWith(version)))
-      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+    clients.openWindow('https://www.minhgiang.pro')
   );
 });
-
-}
-else {
-    console.log('Oops! Workbox did not load');
-}
